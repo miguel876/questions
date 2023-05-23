0aa0b4553cd95d2bbaba2df2212d222915d40961
Miguel <miguel87666@gmail.com>
2023-05-23 16:45:51 +0100

    refactor: changed useFetch conditions and added post and put methods

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,46 +1,133 @@
 import { useEffect, useState } from 'react'
 import properties from '../properties.json'
+interface IResponseData {
+    [key: string]: any,
+};
 
-interface DataI {
-    data: { 
-        [key: string]: any,
-    }, 
-    loading: boolean, 
-    error: any
-}
+interface IErrorData {
+    error: string;
+};
+
+interface IHookResult {
+    loading: boolean;
+    data: IResponseData | null;
+    error: string | null;
+    postRequest?: (url: string, data: any) => Promise<void>;
+    putRequest?: (url: string, data: any) => Promise<void>;
+};
 
 const removeNullParams = (params: any) => {
-    Object.keys(params).forEach((key) => params[key] === null && delete params[key]);
+  Object.keys(params).forEach((key) => params[key] === null && delete params[key]);
 
     return params
 }
 
-export const useGet = (url: string, params?: any, conditions?: any) => {
-    const [{ data, loading, error }, setData ] = useState<DataI>({ data: {}, loading: false, error: {}})
+export const useGet = (url: string, params?: any, condition?: any): IHookResult => {
+    const [loading, setIsLoading] = useState(false);
+    const [data, setResponseData] = useState<IResponseData | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
     const getParams = () => params ? `?${new URLSearchParams(removeNullParams(params))}` : ''
 
-    const getData = async () => {
-        setData((prevData:DataI) => ({...prevData, loading: true}))
+    useEffect(() => {
+        const fetchData = async (): Promise<void> => {
+        setIsLoading(true);
 
         try {
-            const response = await fetch(`${properties.baseURL + url}${getParams()}`)
-            const data = await response.json()
-            setData((prevData:DataI) => ({...prevData, data }))
-        } catch (error) {
-            setData((prevData:DataI) => ({...prevData, error}))
+            const response = await fetch(`${properties.baseURL + url}${getParams()}`);
+
+            if (!response.ok) {
+            const errorData: IErrorData = await response.json();
+            throw new Error(errorData.error);
+            }
+
+            const responseData: IResponseData = await response.json();
+            setResponseData(responseData);
+            setError(null);
+        } catch (error:any) {
+            setError(error.message);
+            setResponseData(null);
         } finally {
-            setData((prevData:DataI) => ({...prevData, loading: false}))
+            setIsLoading(false);
         }
-    }
+        };
 
-    useEffect(() => {
-        getData();
-    }, [url, conditions]);
+        fetchData();
+    }, [url, condition]);
 
-  return ({
-    data,
-    loading,
-    error
-  })
-}
+    return { loading, data, error };
+};
+
+  
+export const usePost = (): IHookResult => {
+    const [loading, setIsLoading] = useState(false);
+    const [data, setResponseData] = useState<IResponseData | null>(null);
+    const [error, setError] = useState<string | null>(null);
+  
+    const postRequest = async (url: string, data: any): Promise<void> => {
+      setIsLoading(true);
+  
+      try {
+        const response = await fetch(`${properties.baseURL + url}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+  
+        if (!response.ok) {
+          const errorData: IErrorData = await response.json() as IErrorData;
+          throw new Error(errorData.error);
+        }
+  
+        const responseData: IResponseData = await response.json();
+        setResponseData(responseData);
+        setError(null);
+      } catch (error:any) {
+        setError(error.message);
+        setResponseData(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
+    return { loading, data, error, postRequest };
+  };
+
+
+  export const usePut = (): IHookResult => {
+    const [loading, setIsLoading] = useState(false);
+    const [data, setResponseData] = useState<IResponseData | null>(null);
+    const [error, setError] = useState<string | null>(null);
+  
+    const putRequest = async (url: string, data: any): Promise<void> => {
+      setIsLoading(true);
+  
+      try {
+        const response = await fetch(`${properties.baseURL + url}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+  
+        if (!response.ok) {
+          const errorData: IErrorData = await response.json() as IErrorData;
+          throw new Error(errorData.error);
+        }
+  
+        const responseData: IResponseData = await response.json();
+        setResponseData(responseData);
+        setError(null);
+      } catch (error:any) {
+        setError(error.message);
+        setResponseData(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
+    return { loading, data, error, putRequest };
+  };
