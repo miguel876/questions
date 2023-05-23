ba63bba12a39c0b4ecf5650aa1b85371e9201364
Miguel <miguel87666@gmail.com>
2023-05-23 16:49:21 +0100

    refactor: added internet connection logic to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from 'layout/Header';
 import Main from 'layout/Main';
 import Footer from 'layout/Footer';
 import { ThemeProvider } from '@mui/system';
 import { theme } from 'theme/theme';
+import { BrowserRouter } from 'react-router-dom';
+import ConnectionScreen from 'pages/ConnectionScreen';
 
 function App() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setIsOnline(true);
+    };
+
+    const handleOffline = () => {
+      setIsOnline(false);
+    };
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
-      <Main />
-      <Footer />
-    </ThemeProvider>
+    <BrowserRouter>
+    
+      <ThemeProvider theme={theme}>
+      {isOnline ? 
+        <>
+          <Header />
+          <Main />
+          <Footer />
+        </>
+       : 
+        <ConnectionScreen />
+       }
+       
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
